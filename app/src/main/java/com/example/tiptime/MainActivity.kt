0950927143c2ff1dda1fc746ef5db95136ea8ec6
Lucos18 <lucabuonpane3@gmail.com>
2022-10-14 13:15:49 +0200

    first commit

---
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.tiptime
+
+import android.content.Context
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.round
+
+lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+
+    }
+
+
+    private fun calculateTip() {
+
+        //Get the user input text inside the EditableText, if empty returns null
+        var cost = binding.costOfServiceEditText.text.toString().toDoubleOrNull()
+        val numberOfPeople = binding.numberOfPeopleEditText.text.toString().toIntOrNull()
+
+        //Check that the cost isn't null
+        if (cost != null) {
+
+            //Check that the cost isn't null and the number of people isn't 0
+            if (numberOfPeople != null && numberOfPeople != 0) {
+
+                //Get the current checked Radio Button to calculate the tips on selected percentage
+                val tipPercentage = returnTipPercentage(binding.tipOptions.checkedRadioButtonId)
+
+                //Calculate the values of the tips and services
+                val tiptotal =
+                    if (binding.roundUpSwitch.isChecked) roundUpDouble(tipPercentage * cost) else tipPercentage * cost
+
+                val tipForPerson =
+                     if (binding.roundUpSwitch.isChecked) roundUpDouble(tiptotal / numberOfPeople) else tiptotal / numberOfPeople
+
+                val totalBillToPayPerPerson = (cost / numberOfPeople) + tipForPerson
+                val totalBillToPay = totalBillToPayPerPerson * numberOfPeople
+                val servicePerPerson = cost / numberOfPeople
+
+                //Format total values of the bills in current currency
+                val formattedTotalBillPerPerson = formatDoubleToCurrency(totalBillToPayPerPerson)
+                val totalBill = formatDoubleToCurrency(totalBillToPay)
+                val formattedTipForPerson = formatDoubleToCurrency(tipForPerson)
+                val formattedServicePerPerson = formatDoubleToCurrency(servicePerPerson)
+
+                //Place in the TextView the calculated prices in formatted currency string
+                binding.totalBill.text = getString(R.string.total_bill, totalBill)
+                binding.totalForPerson.text =
+                    getString(R.string.total_for_person, formattedTotalBillPerPerson)
+                binding.tipForPerson.text =
+                    getString(R.string.tip_for_person, formattedTipForPerson)
+                binding.servicePricePerPerson.text =
+                    getString(R.string.service_price_per_person, formattedServicePerPerson)
+
+            } else showToastErrorMessage(R.string.error_empty_number_of_people)
+        } else showToastErrorMessage(R.string.error_empty_cost_of_service)
+
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
+    private fun returnTipPercentage(checkedRadioButton: Int): Double {
+        return when (checkedRadioButton) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+    }
+
+    private fun showToastErrorMessage(errorMessageId: Int) {
+        Toast.makeText(applicationContext, getString(errorMessageId), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun formatDoubleToCurrency(doubleValue: Double): String {
+        return NumberFormat.getCurrencyInstance().format(doubleValue)
+    }
+
+    private fun roundUpDouble(value: Double): Double {
+        return kotlin.math.ceil(value)
+    }
+}
