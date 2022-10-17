420a98bff0be657f0d596f957de6738e1dd8bfa6
Lucos18 <lucabuonpane3@gmail.com>
2022-10-17 11:44:16 +0200

    Added new toolbar and new icons

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.tiptime
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import java.util.*
@@ -18,14 +20,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_Homepage)
+
+        setSupportActionBar(toolbar)
         binding.calculateButton.setOnClickListener { calculateTip() }
-        binding.Settings.setOnClickListener{ navigateToSettingsActivity() }
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu) : Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_homepage, menu);
+        return true;
     }
 
-    private fun navigateToSettingsActivity(){
-         val intent = Intent(this@MainActivity, SettingsScreenActivity::class.java)
-         startActivity(intent)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.SettingsButton) navigateToSettingsActivity()
+        return super.onOptionsItemSelected(item)
     }
+
+    private fun navigateToSettingsActivity() {
+        val intent = Intent(this@MainActivity, SettingsScreenActivity::class.java)
+        startActivity(intent)
+    }
+
     private fun calculateTip() {
 
         //Get the user input text inside the EditableText, if empty returns null
@@ -38,7 +54,7 @@ class MainActivity : AppCompatActivity() {
             //Check that the cost isn't null and the number of people isn't 0
             if (numberOfPeople != null && numberOfPeople != 0) {
                 var currencyToFormat = Locale.getDefault()
-                var currency = intent.getStringExtra("selectedCurrency");
+                var currency = intent.getStringExtra("selectedCurrency")
 
                 //Get the current checked Radio Button to calculate the tips on selected percentage
                 val tipPercentage = returnTipPercentage(binding.tipOptions.checkedRadioButtonId)
@@ -46,30 +62,33 @@ class MainActivity : AppCompatActivity() {
                 //Calculate the values of the tips and services
                 var tiptotal = tipPercentage * cost
                 var tipForPerson = tiptotal / numberOfPeople
-                if (binding.roundUpSwitch.isChecked)
-                {
+                if (binding.roundUpSwitch.isChecked) {
                     tiptotal = roundUpDouble(tiptotal)
                     tipForPerson = roundUpDouble(tipForPerson)
                 }
                 var totalBillToPayPerPerson = (cost / numberOfPeople) + tipForPerson
                 var totalBillToPay = totalBillToPayPerPerson * numberOfPeople
                 var servicePerPerson = cost / numberOfPeople
+
+                //If currency in the settings is different from null, then convert every value to the selected currency
                 if (currency != null) {
-                    currencyToFormat = trasformCurrencySymbolToLocale(currency)
+                    currencyToFormat = transformCurrencySymbolToLocale(currency)
                     tiptotal = changeCurrencyToSelected(tiptotal, currency)
                     tipForPerson = changeCurrencyToSelected(tipForPerson, currency)
-                    totalBillToPayPerPerson = changeCurrencyToSelected(totalBillToPayPerPerson, currency)
+                    totalBillToPayPerPerson =
+                        changeCurrencyToSelected(totalBillToPayPerPerson, currency)
                     totalBillToPay = changeCurrencyToSelected(totalBillToPay, currency)
                     servicePerPerson = changeCurrencyToSelected(servicePerPerson, currency)
                 }
 
 
                 //Format total values of the bills in current currency
-
-                val formattedTotalBillPerPerson = formatDoubleToCurrency(totalBillToPayPerPerson, currencyToFormat)
+                val formattedTotalBillPerPerson =
+                    formatDoubleToCurrency(totalBillToPayPerPerson, currencyToFormat)
                 val totalBill = formatDoubleToCurrency(totalBillToPay, currencyToFormat)
                 val formattedTipForPerson = formatDoubleToCurrency(tipForPerson, currencyToFormat)
-                val formattedServicePerPerson = formatDoubleToCurrency(servicePerPerson, currencyToFormat)
+                val formattedServicePerPerson =
+                    formatDoubleToCurrency(servicePerPerson, currencyToFormat)
 
                 //Place in the TextView the calculated prices in formatted currency string
                 binding.totalBill.text = getString(R.string.total_bill, totalBill)
@@ -84,38 +103,36 @@ class MainActivity : AppCompatActivity() {
         } else showToastErrorMessage(R.string.error_empty_cost_of_service)
 
     }
-    private fun trasformCurrencySymbolToLocale(currency: String):Locale{
-        return when (currency){
+
+    private fun transformCurrencySymbolToLocale(currency: String): Locale {
+        return when (currency) {
             "$" -> Locale.CANADA
             "€" -> Locale.ITALY
             else -> Locale.getDefault()
         }
     }
+
     private fun returnTipPercentage(checkedRadioButton: Int): Double {
         return when (checkedRadioButton) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
-            R.id.option_fifteen_percent-> 0.15
+            R.id.option_fifteen_percent -> 0.15
             else -> 0.00
         }
     }
 
-    private fun changeCurrencyToSelected(doubleValue: Double, currency: String): Double {
-        var doubleValue2 = doubleValue
+    private fun changeCurrencyToSelected(doubleValue: Double, currencySettings: String): Double {
+        var doubleValueConverted = doubleValue
         val currencyLocale: Currency = Currency.getInstance(Locale.getDefault())
-        Log.d("ciao",currency)
-        when (currencyLocale.symbol){
-            "$" -> if (currency == "€")
-            {
-                doubleValue2 *= 1.03
+        when (currencyLocale.symbol) {
+            "$" -> if (currencySettings == "€") {
+                doubleValueConverted *= 1.03
             }
-            "€" -> if (currency == "$")
-            {
-                doubleValue2 /= 1.03
+            "€" -> if (currencySettings == "$") {
+                doubleValueConverted /= 1.03
             }
-            else -> doubleValue2 + 10000
         }
-        return doubleValue2
+        return doubleValueConverted
     }
 
     private fun showToastErrorMessage(errorMessageId: Int) {
@@ -129,9 +146,9 @@ class MainActivity : AppCompatActivity() {
     private fun roundUpDouble(value: Double): Double {
         return kotlin.math.ceil(value)
     }
+
     fun onRadioButtonClicked(view: View) {
-        if (returnTipPercentage(binding.tipOptions.checkedRadioButtonId) == 0.00)
-        {
+        if (returnTipPercentage(binding.tipOptions.checkedRadioButtonId) == 0.00) {
             binding.roundUpSwitch.isClickable = false
             binding.roundUpSwitch.isChecked = false
         } else binding.roundUpSwitch.isClickable = true
--- app/src/main/java/com/example/tiptime/SettingsScreenActivity.kt
+++ app/src/main/java/com/example/tiptime/SettingsScreenActivity.kt
@@ -2,12 +2,13 @@ package com.example.tiptime
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+
 import androidx.appcompat.app.AppCompatActivity
-import com.example.tiptime.databinding.ActivityMainBinding
+import androidx.appcompat.widget.Toolbar
 import com.example.tiptime.databinding.ActivitySettingsBinding
-import java.text.NumberFormat
 import java.util.*
 
 lateinit var bindingActivity: ActivitySettingsBinding
@@ -18,16 +19,31 @@ class SettingsScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bindingActivity = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(bindingActivity.root)
-        supportActionBar?.title = "Settings Menu"
-        bindingActivity.arrowBackMainActivity.setOnClickListener{ navigateToMainActivity() }
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_Settings)
+        toolbar.title = "Settings Page"
+        setSupportActionBar(toolbar)
+
     }
-    private fun navigateToMainActivity(){
+    override fun onCreateOptionsMenu(menu: Menu) : Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_settings_page, menu);
+        return true;
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.ArrowBackButton) navigateToMainActivity()
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun navigateToMainActivity() {
         val intent = Intent(this@SettingsScreenActivity, MainActivity::class.java)
         intent.putExtra("selectedCurrency", selectedCurrency)
         startActivity(intent)
     }
+
     fun onRadioButtonSettingsClicked(view: View) {
-        when (bindingActivity.currencyOptions.checkedRadioButtonId){
+        when (bindingActivity.currencyOptions.checkedRadioButtonId) {
             R.id.dollar_option -> selectedCurrency = "$"
             R.id.euro_option -> selectedCurrency = "€"
         }
